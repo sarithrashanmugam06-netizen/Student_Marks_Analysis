c622e50483490606c6e07e70713d6e725baefa90
unknown <sarithrashanmmugam.06@gmail.com>
2026-05-26 20:38:55 +0530

    Initial commit

---
+++ Mark_analysis.py
@@ -0,0 +1,22 @@
+import pandas as pd
+
+df= pd.read_csv(r"D:\Datasets\Student_Marks.csv")
+
+print(df)
+
+
+df.describe()
+df.info()
+
+#Average mark
+average = df["Marks"].mean()
+print(average)
+
+#Highest mark
+hight_mark = df["Marks"].max()
+print(hight_mark)
+
+#80 above mark
+
+condition= df["Marks"]>80
+print(condition)
